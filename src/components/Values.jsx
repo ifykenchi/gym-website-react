04b26e476026dce0b7d72988372f62ae7eb24101
ifykenchi <ifykenchi@gmail.com>
2023-09-17 20:46:10 +0100

    finished testimonials

---
+++ src/components/Values.jsx
@@ -0,0 +1,41 @@
+import Image from '../images/values.jpg'
+import SectionHead from './SectionHead'
+import {GiCutDiamond} from 'react-icons/gi'
+import {values} from '../data'
+import Card from '../UI/Card'
+
+
+const Values = () => {
+  return (
+    <section className="values">
+        <div className="container values__container">
+            <div className="values__left">
+                <div className="values__image">
+                    <img src={Image} alt="Values Image" />
+                </div>
+            </div>
+            <div className="values__right">
+                <SectionHead icon={<GiCutDiamond/>} title='Values'/>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere, neque. At earum sint quis, ut maiores quibusdam distinctio ipsam voluptatibus adipisci! Quibusdam dolores, cumque saepe eum placeat officiis quae aut?
+                </p>
+                <div className="values__wrapper">
+                    {
+                        values.map(({id, icon, title, desc}) => {
+                            return (
+                                <Card className="values__value" key={id}>
+                                    <span>{icon}</span>
+                                    <h4>{title}</h4>
+                                    <small>{desc}</small>
+                                </Card>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Values
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,20 @@
-import Header from '../../components/Header'
+import MainHeader from '../../components/MainHeader'
+import Programs from '../../components/Programs'
+import Values from '../../components/Values'
+import FAQs from '../../components/FAQs'
+import Testimonials from '../../components/Testimonials'
 import './home.css'
 
 
 const Home = () => {
   return (
-    <Header/>
+    <>
+    <MainHeader/>
+    <Programs/>
+    <Values/>
+    <FAQs/>
+    <Testimonials/>
+    </>
   )
 }
 
