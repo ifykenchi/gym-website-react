2d469ac844716d4605222f8ffd2693d7e3bf4677
ifykenchi <ifykenchi@gmail.com>
2023-09-23 16:33:29 +0100

    added about page

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,75 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/header_bg_1.jpg'
+import StoryImage from '../../images/about1.jpg'
+import VisionImage from '../../images/about2.jpg'
+import MissionImage from '../../images/about3.jpg'
 import './about.css'
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <Header title='About Us' image={HeaderImage}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque optio possimus veritatis ratione tenetur tempore, amet expedita inventore nihil sunt deserunt illo exercitationem aliquid molestias harum at dignissimos a. Assumenda!
+    </Header>
+
+    <section className="about__story">
+      <div className="container about__story-container">
+        <div className="about__section-image">
+          <img src={StoryImage} alt="Our Story Image" />
+        </div>
+        <div className="about__section-content">
+          <h1>Our Story</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic cumque nostrum animi accusamus fugit nulla ratione corrupti atque aut tenetur veritatis, dicta unde quis placeat tempore esse numquam obcaecati. Dicta!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maiores quia odio corrupti vel dolor enim reprehenderit qui sapiente quas.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, voluptatum.
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <section className="about__Vision">
+      <div className="container about__Vision-container">
+        <div className="about__section-content">
+          <h1>Our Vision</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic cumque nostrum animi accusamus fugit nulla ratione corrupti atque aut tenetur veritatis, dicta unde quis placeat tempore esse numquam obcaecati. Dicta!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maiores quia odio corrupti vel dolor enim reprehenderit qui sapiente quas.
+          </p>
+        </div>
+        <div className="about__section-image">
+          <img src={VisionImage} alt="Our Vision Image" />
+        </div>
+      </div>
+    </section>
+
+    <section className="about__mission">
+      <div className="container about__mission-container">
+        <div className="about__section-image">
+          <img src={MissionImage} alt="Our Mission Image" />
+        </div>
+        <div className="about__section-content">
+          <h1>Our Mission</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic cumque nostrum animi accusamus fugit nulla ratione corrupti atque aut tenetur veritatis, dicta unde quis placeat tempore esse numquam obcaecati. Dicta!
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores maiores quia odio corrupti vel dolor enim reprehenderit qui sapiente quas.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, voluptatum.
+          </p>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
